a862093eae824c15e199cdeb9f80faa4ebef172b
L <121696146+Henreh1@users.noreply.github.com>
2026-04-05 21:47:07 +0100

    changed: changed resolve playlist and fetch track to YTDL sharp dep

--- YtDlpBridge.cs
+++ YtDlpBridge.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Reflection;
 using YoutubeDLSharp;
 using YoutubeDLSharp.Metadata;
 using YoutubeDLSharp.Options;
